9f3cf3064702cbf4bc16bb4e47660dbd5717c3ca
xpcrtv <20889274+xpcrtv@users.noreply.github.com>
2020-06-26 22:57:13 +0500

    add card component and sample dataset

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Header from "./components/Header";
 import HeaderBlock from "./components/HeaderBlock";
 import TextBlock from "./components/TextBlock";
 import BgImageBlock from "./components/BgImageBlock";
+import Card from "./components/Card";
+
+import { wordsList } from "./data/wordsList";
 
 import Footer from "./components/Footer";
 import logo from "./logo.svg";
@@ -15,6 +18,9 @@ const App = () => {
         title="Учите слова онлайн"
         description="Воспользуйтесь карточками для запоминания и пополнения активныйх словарных запасов"
       />
+      {wordsList.map(({ eng, rus }, idx) => (
+        <Card key={idx} eng={eng} rus={rus} />
+      ))}
       <TextBlock
         text="Общий текст для блока с общим текстом"
         fontSize="30px"
