d7493c617e8fb4b815fa599d4f95ea0375e3d3ff
xpcrtv <20889274+xpcrtv@users.noreply.github.com>
2020-06-27 19:39:01 +0500

    fix utility helper

--- src/utils/shuffleArray.js
+++ src/utils/shuffleArray.js
@@ -1,6 +1,6 @@
 export default (array) => {
-  let shuffledArray = array;
-  for (let i = array.length - 1; i > 0; i--) {
+  let shuffledArray = [...array];
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
     [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
   }
