815b1fa9820567b76e3e6a3695d2e7bef247aa69
xpcrtv <20889274+xpcrtv@users.noreply.github.com>
2020-06-29 23:06:22 +0500

    add logic for adding word cards. add styles to card list component. rewrite components from functional style to classes.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "./components/Header";
 import HeaderBlock from "./components/HeaderBlock";
 import Paragraph from "./components/Paragraph";
@@ -16,26 +16,58 @@ import logo from "./logo.svg";
 import subscribeImg from "./assets/img/449.jpg";
 import cardsImg from "./assets/img/450.jpg";
 
-const App = () => {
-  return (
-    <>
-      <Header logoSrc={logo} title="Learn words" />
-      <HeaderBlock title="Учите слова онлайн">
-        <Paragraph color="white">
-          Воспользуйтесь карточками для запоминания и пополнения активныйх
-          словарных запасов
-        </Paragraph>
-      </HeaderBlock>
-      <FeaturesList data={features} />
-      <BgImageBlock imgSrc={cardsImg} bgcSize="cover">
-        <CardList data={words} />
-      </BgImageBlock>
-      <BgImageBlock imgSrc={subscribeImg} bgcSize="cover">
-        <SubscribeBlock />
-      </BgImageBlock>
-      <Footer text={`Learn words | ${new Date().getFullYear()}`} />
-    </>
-  );
-};
+class App extends Component {
+  state = {
+    words,
+    features,
+  };
+
+  deleteWord = (id) => {
+    this.setState(({ words }) => {
+      const deletedWordIndex = words.findIndex((word) => word.id === id);
+      const filtredWords = words.filter(
+        (word, idx) => idx !== deletedWordIndex
+      );
+      return {
+        words: filtredWords,
+      };
+    });
+  };
+
+  addWord = (word) => {
+    this.setState(({ words }) => {
+      return {
+        words: [...words, word],
+      };
+    });
+  };
+
+  render() {
+    const { words, features } = this.state;
+    return (
+      <>
+        <Header logoSrc={logo} title="Learn words" />
+        <HeaderBlock title="Учите слова онлайн">
+          <Paragraph color="white">
+            Воспользуйтесь карточками для запоминания и пополнения активныйх
+            словарных запасов
+          </Paragraph>
+        </HeaderBlock>
+        <FeaturesList data={features} />
+        <BgImageBlock imgSrc={cardsImg} bgcSize="cover">
+          <CardList
+            data={words}
+            onDeletedItem={this.deleteWord}
+            onAddItem={this.addWord}
+          />
+        </BgImageBlock>
+        <BgImageBlock imgSrc={subscribeImg} bgcSize="cover">
+          <SubscribeBlock />
+        </BgImageBlock>
+        <Footer text={`Learn words | ${new Date().getFullYear()}`} />
+      </>
+    );
+  }
+}
 
 export default App;
