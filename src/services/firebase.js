c8886129d467cf3b7da45665e5f3ffcb64bb35ea
xpcrtv <20889274+xpcrtv@users.noreply.github.com>
2020-07-01 22:26:46 +0500

    add services for translate words and work with firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,32 @@
+import * as firebase from "firebase/app";
+
+import "firebase/database";
+
+const {
+  REACT_APP_DB_API_KEY,
+  REACT_APP_AUTHDOMAIN,
+  REACT_APP_DATABASEURL,
+  REACT_APP_PROJECTID,
+  REACT_APP_STORAGEBUCKET,
+  REACT_APP_MESSAGINGSENDERID,
+  REACT_APP_APPID,
+} = process.env;
+
+const firebaseConfig = {
+  apiKey: REACT_APP_DB_API_KEY,
+  authDomain: REACT_APP_AUTHDOMAIN,
+  databaseURL: REACT_APP_DATABASEURL,
+  projectId: REACT_APP_PROJECTID,
+  storageBucket: REACT_APP_STORAGEBUCKET,
+  messagingSenderId: REACT_APP_MESSAGINGSENDERID,
+  appId: REACT_APP_APPID,
+};
+
+class Firebase {
+  constructor() {
+    firebase.initializeApp(firebaseConfig);
+    this.db = firebase.database();
+  }
+}
+
+export default Firebase;
